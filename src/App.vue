71bfc1bec83b189a26438553c51abde3aa8fc390
Gaaraly <precious.wei@qq.com>
2023-04-10 02:27:06 +0800

    repo init

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <main h-screen font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <RouterView />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+})
+</script>
+
+<template>
+  <div h-full flex justify-center items-center>
+    <input
+      w-100 lg:w-200 px-4 py-2 text-5 bg-gray bg-op20
+      rounded border outline-none ring
+      type="text"
+      autocomplete="off"
+    >
+  </div>
+</template>
